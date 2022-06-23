46dca57a9d01cab2dee25b2af0b1979bb9f72ee7
Matielota <matias_vll@hotmail.com>
2022-06-23 17:40:03 -0300

    asd

---
+++ index.js
@@ -0,0 +1,53 @@
+const io = require ("socket.io")(3005 ,{
+    cors:{
+        origin:"http://localhost:3000"
+    }
+});
+
+io.on("conection", (socket)=>{
+    console.log("a user conected.")
+})
+
+ let users= [];
+
+const addUser = (userId, socketId) =>{
+    !users.some((user) => user.userId === userId) &&
+    users.push({userId, socketId});
+}
+
+const removeUser = (socketId) =>{
+    users = users.filter((user)=>user.socketId !== socketId)
+}
+
+const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+  };
+
+
+io.on("connection", (socket) => {
+    console.log("a user conectiojn",socket.id);
+    socket.on("addUser", (userId)=>{
+        addUser(userId, socket.id);
+        io.emit("getUsers", users);
+    });
+    socket.on("disconnect", ()=>{
+        console.log("a user disconnected", socket.id);
+        removeUser(socket.id);
+        io.emit("getUsers", users);
+    });
+    //send and get message
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+        try{
+        const user = getUser(receiverId);
+        console.log(user)
+        console.log(senderId,
+            text,)
+        io.to(user.socketId).emit("getMessage", {
+          senderId,
+          text,
+        });}
+        catch(err){
+            console.log(err)
+        }
+      });
+}) 
